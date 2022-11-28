8a80e890282600d716bbdd625d2a0e978aea2826
Rohan Pahwa <71563384+rohanphw@users.noreply.github.com>
2022-11-28 11:13:21 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -103,6 +103,33 @@ export default function Home() {
           </button>
         </div>
       </div>
+      <div className="absolute inset-x-0 bottom-0 -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
+        <svg
+          className="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]"
+          viewBox="0 0 1155 678"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill="url(#ecb5b0c9-546c-4772-8c71-4d3f06d544bc)"
+            fillOpacity=".3"
+            d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
+          />
+          <defs>
+            <linearGradient
+              id="ecb5b0c9-546c-4772-8c71-4d3f06d544bc"
+              x1="1155.49"
+              x2="-78.208"
+              y1=".177"
+              y2="474.645"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop stopColor="#9089FC" />
+              <stop offset={1} stopColor="#FF80B5" />
+            </linearGradient>
+          </defs>
+        </svg>
+      </div>
       <div className="flex flex-col justify-center items-center p-2 mt-4 lg:mt-12">
         <h1 className="text-3xl md:text-6xl lg:text-6xl mt-5 lg:mt-0 p-4 font-bold text-gray-800">
           What&apos;s on your mind ?
@@ -114,12 +141,13 @@ export default function Home() {
       <div className="pt-[10px] p-2">
         {error && <div>{error}</div>}
         <div className="border-hairline max-w-[512px]  lg:p-0 mx-auto relative rounded-3xl">
-          <div className="bg-transparent max-h-[455px] w-full flex items-stretch rounded-lg border-gray-600">
+          <div className="bg-transparent max-h-[455px] lg:max-h-[550px] md:max-h-[455px] w-full flex items-stretch rounded-lg border-gray-600">
             <Canvas
               predictions={predictions}
               userUploadedImage={userUploadedImage}
               onDraw={setMaskImage}
             />
+
             <div className="flex lg:hidden md:hidden">
               <TopBanner />
             </div>
