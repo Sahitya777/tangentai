a43a7d02e47af153bea356ebdf2ec1f2de6624ae
Rohan Pahwa <71563384+rohanphw@users.noreply.github.com>
2022-12-06 21:31:41 +0530

    price update

--- context/config.js
+++ context/config.js
@@ -1,7 +1,7 @@
 const config = {
   //mintNFT contract
   contractAddress: "KT1W1SjHMYD8HgkB6gQFPbHDqCEFn3PqUpsJ",
-  NFT_PRICE: 1,
+  NFT_PRICE: 3,
   GHOSTNET_RPC: "https://ghostnet.smartpy.io",
   MAINNET_RPC: "https://rpc.tzbeta.net",
 };
